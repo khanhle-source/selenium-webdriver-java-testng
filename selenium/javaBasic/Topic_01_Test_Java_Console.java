0efc03067b7800026492e875942fcf5527308dfe
hubblemacbookpro <hubblemacbookpro@localhost>
2022-01-22 21:58:46 +0700

    add comment

--- selenium/javaBasic/Topic_01_Test_Java_Console.java
+++ selenium/javaBasic/Topic_01_Test_Java_Console.java
@@ -3,6 +3,7 @@ package javaBasic;
 public class Topic_01_Test_Java_Console {
 
 	public static void main(String[] args) {
+		// add comment 
 		String projectPath = System.getProperty("user.dir");
 		System.out.print(projectPath);
 
